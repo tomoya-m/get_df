787c5ad9f9fa8e7701cf53aae07b3e08996631e7
tomoya-m <95002334+tomoya-m@users.noreply.github.com>
2022-02-21 10:07:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="get_df",
     version="0.1",
     install_requires=["pandas"]
+    packages=find_packages()
 )
